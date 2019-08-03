0ed73862b039f62e263802e416cac4eabe23b12c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 04:01:37 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -5,10 +5,18 @@ def initialize(sighting_object)
 end
 
 def to_serialized_json
-  @sighting.to_json(:include => {
-    :bird => {:only => [:name, :species]},
-    :location => {:only => [:latitude, :longitude]}
-  }, :except => [:updated_at])
+  options = {
+    include: {
+      bird: {
+        only: [:name, :species]
+      },
+      location: {
+        only: [:latitude, :longitude]
+      }
+    },
+    except: [:updated_at],
+  }
+  @sighting.to_json(options)
 end
 
 end
